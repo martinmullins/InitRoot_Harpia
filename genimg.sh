1585ef4f7f5b35851bf51f1a7fb4230ab5728a16
Martin Mullins <martin.mullins942@gmail.com>
2017-09-15 19:20:33 -0700

    Add optiont to create unpadded gpio for untethered root

--- genimg.sh
+++ genimg.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+IMAGE_FOLDERS="oem sys system proc dev data customize dsp"
+
 # to allow empty folders in the github, recreate them before pushing
-recreateIgnores() {
+recreate_gitignore() 
+{
     cd initrdimg-harpia/ 
-    touch {oem,sys,system,proc,dev,data,customize,dsp}/.gitignore
+    for i in $IMAGE_FOLDERS; do
+        touch $i/.gitignore
+    done
     cd -
 }
 
-harpia() {
-    #remove .gitignores
-    for i in oem add sys system proc dev data customize dsp; do
+remove_gitignore() 
+{
+    for i in $IMAGE_FOLDERS; do
         gitig="initrdimg-harpia/$i/.gitignore"
         if [[ -e $gitig ]]; then
             echo -n "Git doesn't allow empty directores, removing temporary file: "
             rm -v $gitig
         fi
     done
+}
+
+harpia() 
+{
+    remove_gitignore
 
     if [[ ! -e pad64 ]]; then
         dd if=/dev/zero of=pad64  bs=1M  count=64
@@ -28,6 +38,25 @@ harpia() {
     rm -v ../tmp
     cd -
     echo "initrd size is $SZ"
+
+
+    recreate_gitignore
+}
+
+harpia_nopad()
+{
+    remove_gitignore
+
+    cd initrdimg-harpia/ 
+    find . | grep -v [.]$ | cpio -v -R root:root -o -H newc | gzip > ../tmp 
+    SZ=$(awk '{ print $5; }' <(ls -al ../tmp))
+    cat ../tmp > ../initroot-harpia-nopad.cpio.gz 
+    rm -v ../tmp
+    cd -
+    echo "initrd size is $SZ"
+
+
+    recreate_gitignore
 }
 
 $*
